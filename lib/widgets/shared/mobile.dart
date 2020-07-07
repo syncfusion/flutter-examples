e196882d245bdb75dc0b88503ae4d52e5fadd795
yuvaraj.gajaraj <yuvaraj.gajaraj@syncfusion.com>
2020-07-07 16:40:36 +0530

    changes.

--- lib/widgets/shared/mobile.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class HandCursor extends MouseRegion {
-  //ignore: prefer_const_constructors_in_immutables
-  HandCursor({@required Widget child}) : super(child: child);
-}
-
-void changeCursorStyleOnNavigation() {}
