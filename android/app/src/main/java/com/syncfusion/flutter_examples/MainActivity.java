18b260c1ce2c12d5339b758f6fbd3040c872c61c
yuvaraj.gajaraj <yuvaraj.gajaraj@syncfusion.com>
2020-10-01 23:55:10 +0530

    New samples added

--- android/app/src/main/java/com/syncfusion/flutter_examples/MainActivity.java
+++ android/app/src/main/java/com/syncfusion/flutter_examples/MainActivity.java
@@ -13,30 +13,31 @@ import androidx.core.content.PermissionChecker;
 
 import java.io.File;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugin.common.MethodCall;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-    new MethodChannel(getFlutterView(), "launchFile").setMethodCallHandler(new MethodChannel.MethodCallHandler() {
-      @Override
-      public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-        if (call.method.equals("viewPdf") || call.method.equals("viewExcel")) {
-          String path = call.argument("file_path");
-          if(!checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
-            requestPermission(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE});
-          } else {
-            launchFile(path);
-          }
-        }
-      }
-    });
-  }
+  
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "launchFile")
+                .setMethodCallHandler(
+                    (call, result) -> {
+                      if (call.method.equals("viewPdf") || call.method.equals("viewExcel")) {
+                        String path = call.argument("file_path");
+                        if(!checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                          requestPermission(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE});
+                        } else {
+                          launchFile(path);
+                        }
+                      }
+                }
+        );
+    }
   private void requestPermission(String[] permission){
     ActivityCompat.requestPermissions(this, permission, 1);
   }
