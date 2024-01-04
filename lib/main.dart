b395b6c9dc4dff50b1e5c01a88606107b3fe6241
Yuvaraj-Gajaraj <yuvaraj.gajaraj@syncfusion.com>
2024-01-04 16:48:25 +0530

    Removed old source

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'model/model.dart';
-import 'sample_browser.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await updateControlItems();
-  runApp(const SampleBrowser());
-}
