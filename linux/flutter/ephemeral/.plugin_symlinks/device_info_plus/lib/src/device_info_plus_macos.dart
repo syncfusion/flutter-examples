9732af1cdfdcc70545f15c0e5001c5745828da30
natrayansf <natrayan.ramalingam@syncfusion.com>
2025-12-15 12:13:49 +0530

    Kept server permission changes and removed unwanted changes

--- linux/flutter/ephemeral/.plugin_symlinks/device_info_plus/lib/src/device_info_plus_macos.dart
+++
@@ -1,2 +0,0 @@
-// ignore: dangling_library_doc_comments
-/// The platform interface handles the method channel calls. This file exists to silence errors on pub.
--- linux/flutter/ephemeral/.plugin_symlinks/syncfusion_pdfviewer_linux/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-## Unreleased
-
-No changes.
-
-## [30.1.37] - 06/25/2025
-
-* Initial release.
